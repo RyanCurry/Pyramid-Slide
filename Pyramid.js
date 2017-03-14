8ccf391dc2c5f13a6516054735c8f09314eab56d
Ryan Curry <rtc1995@yahoo.com>
2017-03-13 19:18:47 -0500

    changed space char from . to nbsp, moved slider into a div, added border to slider div, added h1 tag, added title to head

--- Pyramid.js
+++ Pyramid.js
@@ -1,24 +1,41 @@
 var slider=document.getElementById("pyramidRange");
 var HeightNum=document.getElementById("PyramidHeight");
+var PyramidContainer=document.getElementById("pyramid");
+var BrickCharSelect=document.getElementById("BrickCharSelect");
+
+OnInput() //make first pyramid
+
+BrickCharSelect.addEventListener("change",OnInput);
+slider.addEventListener("input",OnInput);
 
 
-slider.addEventListener("input",function(event) {
-    HeightNum.innerHTML=slider.value
-})
+function RemovePyramid() {
+    PyramidContainer.innerHTML=""
+}
+function OnInput(){
+    HeightNum.innerHTML=slider.value;
+    RemovePyramid();
+    makePyramid(slider.value,BrickCharSelect.value,"&nbsp");
+}
 
-function makePyramid(height,char) {
-    for (var row=0;row<height;row++) {
-        var blocks=row + 2
-        var spaces=height - row - 1
+function makePyramid(height,BrickChar,SpaceChar) {
 
-        rowStr=""
-        for (spaces;spaces>0;spaces-=1) {
-            rowStr+="."
+    for (var row = 0; row < height; row++) {
+        var rowStr = "";
+        var bricks = row + 2;
+        var spaces = height - row - 1;
+
+        for (var i = 0; i < spaces; i++) {
+            rowStr += SpaceChar;
+        }
+        for (var i = 0; i < bricks; i++) {
+            rowStr += BrickChar;
         }
 
-        for (blocks;blocks>0;blocks-=1) {
-            rowStr+=char
+            PyramidElem=document.createElement("p");
+            PyramidElem.innerHTML=rowStr;
+            PyramidContainer.appendChild(PyramidElem);
+
         }
 
-    }
 }
